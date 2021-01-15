6e271403df838bec71fa39ab1c1218dfa5523fde
Nipun Garg <nipung271@gmail.com>
2021-01-15 17:34:55 +0530

    Add connectToWebSocket()

---
+++ src/websocket.js
@@ -0,0 +1,13 @@
+import ipRegex from "ip-regex";
+
+export default function connectToWebSocket(address, port, endpoint, parser) {
+  if (!ipRegex({ exact: true, includeBoundaries: true }).test(address)) {
+    console.log("Invalid IP address: " + address);
+    return;
+  }
+
+  const socketURL = ["ws://", address, ":", port, "/", endpoint].join("");
+
+  const socket = new WebSocket(socketURL);
+  socket.onmessage = parser;
+}

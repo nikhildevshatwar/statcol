13d9200a7e8b645531f92699fa504fed9bbf28c3
Nipun Garg <nipung271@gmail.com>
2021-01-21 00:50:41 +0530

    Add argsToString() and add to connectToWebsocket()

--- src/websocket.js
+++ src/websocket.js
@@ -1,6 +1,39 @@
 import ipRegex from "ip-regex";
 
-export default function connectToWebSocket(address, port, endpoint, parser) {
+function argsToString(args) {
+  /* 
+    argsToString expects args to be an Object of
+    key-value pairs where each value is either a
+    Number or a String.
+  */
+  if (args === {}) {
+    return "";
+  }
+
+  const pairToString = (key, value) => {
+    if (typeof value === "number") {
+      return `${key}=${value}`;
+    } else {
+      return `${key}=${value.replace(/\s+/g, " ").trim().replace(" ", "%20")}`;
+    }
+  };
+
+  let encodedString = "";
+  for (const [key, value] of Object.entries(args)) {
+    encodedString += pairToString(key, value);
+    encodedString += "&&";
+  }
+
+  return encodedString.substring(0, encodedString.length - 2);
+}
+
+export default function connectToWebSocket(
+  address,
+  port,
+  endpoint,
+  parser,
+  args = {}
+) {
   if (
     !ipRegex({ exact: true, includeBoundaries: true }).test(address) &&
     address !== "localhost"
@@ -9,7 +42,16 @@ export default function connectToWebSocket(address, port, endpoint, parser) {
     return;
   }
 
-  const socketURL = ["ws://", address, ":", port, "/", endpoint].join("");
+  const socketURL = [
+    "ws://",
+    address,
+    ":",
+    port,
+    "/",
+    endpoint,
+    "?",
+    argsToString(args),
+  ].join("");
 
   const socket = new WebSocket(socketURL);
   socket.onmessage = parser;

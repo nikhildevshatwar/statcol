83ce98e6f6129b173c15e0eb8bf59f9da2f0c5fe
Nipun Garg <nipung271@gmail.com>
2021-01-27 11:05:14 +0530

    Add PieChart component

---
+++ src/components/PieChart.js
@@ -0,0 +1,38 @@
+import { makeStyles } from "@material-ui/core/styles";
+import { colors } from "../globals";
+import React from "react";
+import Plot from "react-plotly.js";
+
+const useStyles = makeStyles({
+  root: {
+    margin: 20,
+    boxShadow: "0 2px 2px 0px",
+  },
+});
+
+export default function PieChart(props) {
+  const classes = useStyles();
+
+  return (
+    <Plot
+      className={classes.root}
+      data={props.data.map(({ name, values, labels }) => ({
+        name: name,
+        values: values,
+        labels: labels,
+        type: "pie",
+      }))}
+      layout={{
+        legend: { bgcolor: colors.container },
+        font: { color: colors.text },
+        margin: {
+          pad: 30,
+        },
+        autosize: true,
+        paper_bgcolor: colors.container,
+        plot_bgcolor: colors.plot,
+        uirevision: 1,
+      }}
+    ></Plot>
+  );
+}

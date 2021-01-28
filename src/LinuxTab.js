6f31a212874c2127b5abd2d3690005e5178ada14
Nipun Garg <nipung271@gmail.com>
2021-01-28 14:54:06 +0530

    Fix import

--- src/LinuxTab.js
+++ src/LinuxTab.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/table";
+import Table from "@material-ui/core/Table";
 import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
